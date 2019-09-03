149717c832ee7baf887a1c461b27f0497260de4f
Taran <thetaranneeld@gmail.com>
2019-09-03 13:08:08 -0600

    rendering my github data, and grabbing follower data

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
+import "./App.css";
+import UserCard from "./components/UserCard";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      user: {}
+      user: {},
+      followers: []
     };
   };
 
   componentDidMount() {
-    axios.get('https://api.github.com/users/taranmneeld')
+    axios.get("https://api.github.com/users/taranmneeld")
       .then(res => this.setState({
-        user: res
+        user: res.data
+      },))
+      .catch(err => console.log(err));
+      axios.get("https://api.github.com/users/taranmneeld/followers")
+      .then(res => this.setState({
+        followers: res.data
       }))
       .catch(err => console.log(err));
   }
 
   render() {
     return (
-      <div>
-        {console.log(this.state.user)}
+      <div className="App">
+        {<UserCard
+          imgUrl={this.state.user.avatar_url}
+          name={this.state.user.name}
+          username={this.state.user.login}
+          bio={this.state.user.bio}
+          profile={this.state.user.blog}
+        />}
       </div>
     );
   }
--- user-card/src/components/UserCard.js
+++ user-card/src/components/UserCard.js
@@ -1,10 +1,23 @@
 import React from "react";
+import "semantic-ui-css/semantic.min.css";
+import { Card, Icon, Image } from 'semantic-ui-react'
 
 const UserCard = props => {
     return (
-        <div>
-           <img></img>
-        </div>
+        <Card>
+        <Image src={props.imgUrl} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>{props.name}</Card.Header>
+          <Card.Meta>{props.username}</Card.Meta>
+          <Card.Description>{props.bio}</Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <a href={props.profile}>
+            <Icon name='linkify' />
+            Profile
+          </a>
+        </Card.Content>
+      </Card>
     );
 };
 
